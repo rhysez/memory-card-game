9ce9086324ca92807f13a3b66cf0d443666dcef6
rhysez <121772484+rhysez@users.noreply.github.com>
2023-08-29 23:38:53 +0100

    implement parsing of json into object to retrieve pokemon data and display to user in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
 export default function Card(props) {
+  const [pokemonAvatar, setPokemonAvatar] = useState(null);
+  const [pokemonName, setPokemonName] = useState('');
 
-    const [pokemonName, setPokemonName] = useState('')
+  const pokemonList = [
+    "bulbasaur",
+    "squirtle",
+    "charizard",
+    "snorlax",
+    "oddish",
+    "pikachu",
+    "magikarp",
+    "flareon",
+    "diglett",
+  ];
 
-    async function fetchPokemon() {
-        const pokemonCard = document.querySelector('.pokemonCard')
+  const randomPokemon = Math.floor(Math.random() * pokemonList.length)
 
-        try {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-            const pokemonData = await response.json()
-            console.log(pokemonData.name)
-            setPokemonName(pokemonData.name)
-        }
-    
-        catch {
-            console.log('Could not fetch pokemon')
-        }
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${randomPokemon}`
+
+  async function fetchPokemon() {
+    try {
+      const response = await fetch(pokemonUrl);
+      const pokemonData = await response.json();
+      const pokemonNameCapitalised = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
+      console.log(pokemonData);
+      setPokemonName(pokemonNameCapitalised)
+      setPokemonAvatar(pokemonData.sprites.front_default);
+    } 
+    catch {
+      throw new Error("Could not fetch pokemon!");
     }
+  }
 
+  const returnedPokemon = fetchPokemon();
 
-    return (
-        <>
-            <button onClick={fetchPokemon}>Click for Pokemon</button>
-            <div className='pokemonCard'>{pokemonName}</div>
-        </>
-    )
+  return (
+    <>
+      <div className="pokemonCard">
+        <span>{pokemonName}</span>
+        <img src={pokemonAvatar}></img>
+      </div>
+    </>
+  );
 }
