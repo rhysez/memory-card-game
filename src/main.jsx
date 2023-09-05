5a9fc56f06f5ea5e5bc32ac38538d4f893ad9405
rhysez <121772484+rhysez@users.noreply.github.com>
2023-09-05 16:37:15 +0100

    no notable changes made

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App className='app'/>
+    <App/>
   </React.StrictMode>,
 )
